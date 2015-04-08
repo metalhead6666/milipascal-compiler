e648d5e1e47835651aec4e3740087f832ac16a6e
metalhead6666 <noe.godinho.92@gmail.com>
2015-04-08 15:53:45 +0100

    C and header of the functions to create the abstract syntax tree and print it.

--- Phase 2/functions.c
+++ Phase 2/functions.c
@@ -1,9 +1,21 @@
 #include "functions.h"
 
-Program* makeNode(){
-	
+Program* makeNode(char* id){
+	Program* prog = (Program *)malloc(sizeof(Program));
+	//prog->progBlock = progBlock;
+	strcpy(prog->id, id);
+
+	return prog;
 }
 
-void print_tree(){
+ProgBlock* addProgBlock(VarPart* varPart, FuncPart* funcPart){
+	ProgBlock* progBlock = (ProgBlock *)malloc(sizeof(ProgBlock));
+	progBlock->varPart = varPart;
+	progBlock->funcPart = funcPart;
 
+	return progBlock;
+}
+
+void print_tree(Program* program){
+	printf("%s\n", program->id);
 }
--- Phase 2/functions.h
+++ Phase 2/functions.h
@@ -1,4 +1,14 @@
+#ifndef _FUNCTIONS_
+#define _FUNCTIONS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "structures.h"
 
-Program* makeNode();
-void print_tree();
+Program* makeNode(char* id);
+ProgBlock* addProgBlock(VarPart* varPart, FuncPart* funcPart);
+
+void print_tree(Program* program);
+
+#endif
