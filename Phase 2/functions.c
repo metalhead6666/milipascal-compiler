c91c6b1d28003a1d5b4817c8b5594ce22ad75e87
Christophe235 <christophe_1011@hotmail.com>
2015-04-09 01:35:19 +0100

    criação das funções das seguintes estruturas: /* *	IdStruct *	VarParams *	Params *	VarDecl *	VarPart *	ProgBlock *	Program *	FuncParams *		|FuncParams_Params *		|FuncParams_VarParams *	FuncDecl *	FuncDef * 	FuncDef2 *	FuncPart *		|FuncPart_FuncDecl *		|FuncPart_FuncDef *		|FuncPart_FuncDef2 */

--- Phase 2/functions.c
+++ Phase 2/functions.c
@@ -1,6 +1,26 @@
 #include "functions.h"
 
-Program* makeNode(char* id, ProgBlock* progBlock){
+/*
+*	IdStruct
+*	VarParams
+*	Params
+*	VarDecl
+*	VarPart
+*	ProgBlock
+*	Program
+*	FuncParams
+*		|FuncParams_Params
+*		|FuncParams_VarParams
+*	FuncDecl
+*	FuncDef
+* 	FuncDef2
+*	FuncPart
+*		|FuncPart_FuncDecl
+*		|FuncPart_FuncDef
+*		|FuncPart_FuncDef2
+*/
+
+Program *makeNode(char* id, ProgBlock* progBlock){
 	Program* prog = (Program *)malloc(sizeof(Program));
 	prog->progBlock = progBlock;
 	prog->id = id;
@@ -8,12 +28,121 @@ Program* makeNode(char* id, ProgBlock* progBlock){
 	return prog;
 }
 
-ProgBlock* addProgBlock(VarPart* varPart, FuncPart* funcPart){
+IdStruct *addIdStruct(IdStruct* next, char *id){
+	IdStruct *idStruct = (IdStruct *)malloc(sizeof(IdStruct));
+	idStruct->next = next;
+	idStruct->id = id;
+
+	return idStruct;
+}
+
+VarParams *addVarParams(char *first_id, char *last_id, IdStruct *id){
+	VarParams *varParams = (VarParams *)malloc(sizeof(VarParams));
+	varParams->first_id = first_id;
+	varParams->last_id = last_id;
+	varParams-> id = id;
+
+	return varParams;
+}
+
+Params *addParams(char *first_id, char *last_id, IdStruct *id){
+	Params *params = (Params *)malloc(sizeof(Params));
+	params->first_id = first_id;
+	params->last_id = last_id;
+	params-> id = id;
+
+	return params;
+}
+
+ProgBlock *addProgBlock(VarPart* varPart, FuncPart* funcPart){
 	ProgBlock* progBlock = (ProgBlock *)malloc(sizeof(ProgBlock));
 	progBlock->varPart = varPart;
 	progBlock->funcPart = funcPart;
 
 	return progBlock;
 }
+
+VarPart *addVarPart(VarPart *varPart, VarDecl *varDecl){
+	VarPart *new_varPart = (VarPart *)malloc(sizeof(VarPart));
+	new_varPart->varDecl = varDecl;
+	new_varPart->next = varPart;
+
+	return new_varPart;
+}
+
+VarDecl *addVarDecl(char *first_id, char *last_id, IdStruct *id){
+	VarDecl *varDecl = (VarDecl *)malloc(sizeof(VarDecl));
+	varDecl->first_id = first_id;
+	varDecl->last_id = last_id;
+	varDecl-> id = id;
+
+	return varDecl;
+}
+
+FuncParams *addFuncParams_Params(Params *params){
+	FuncParams *funcParams = (FuncParams *)malloc(sizeof(FuncParams));
+	funcParams->OptionalParams.params=params;
+
+	return funcParams;
+}
+
+FuncParams *addFuncParams_VarParams(VarParams *varParams){
+	FuncParams *funcParams = (FuncParams *)malloc(sizeof(FuncParams));
+	funcParams->OptionalParams.varParams=varParams;
+
+	return funcParams;
+}
+
+FuncDecl *addFuncDecl(char *first_id, char *last_id, FuncParams *funcParams){
+	FuncDecl *funcDecl = (FuncDecl *)malloc(sizeof(FuncDecl));
+	funcDecl->first_id = first_id;
+	funcDecl->last_id = last_id;
+	funcDecl-> funcParams = funcParams;
+
+	return funcDecl;
+}
+
+FuncDef *addFuncDef(FuncDecl *funcDecl, VarPart *varPart, Statements stat){
+	FuncDef *funcDef = (FuncDef *)malloc(sizeof(FuncDef));
+	funcDef->funcDecl = funcDecl;
+	funcDef->varPart = varPart;
+	funcDef->stat = stat;
+
+	return funcDef;
+}
+
+FuncDef2 *addFuncDef2(char *id, VarPart *varPart, Statements stat){
+	FuncDef2 *funcDef = (FuncDef2 *)malloc(sizeof(FuncDef2));
+	funcDef->id = id;
+	funcDef->varPart = varPart;
+	funcDef->stat = stat;
+
+	return funcDef;
+}
+
+FuncPart *addFuncPart_FuncDecl(FuncPart *next, FuncDecl *funcDecl){
+	FuncPart *funcPart = (FuncPart *)malloc(sizeof(FuncPart));
+	funcPart->next = next;
+	funcPart->funcPart.funcDecl = funcDecl;
+
+	return funcPart;
+}
+
+FuncPart *addFuncPart_FuncDef(FuncPart *next, FuncDef *funcDef){
+	FuncPart *funcPart = (FuncPart *)malloc(sizeof(FuncPart));
+	funcPart->next = next;
+	funcPart->funcPart.funcDef = funcDef;
+
+	return funcPart;
+}
+
+FuncPart *addFuncPart_FuncDef2(FuncPart *next, FuncDef2 *funcDef2){
+	FuncPart *funcPart = (FuncPart *)malloc(sizeof(FuncPart));
+	funcPart->next = next;
+	funcPart->funcPart.funcDef2 = funcDef2;
+
+	return funcPart;
+}
+
 
 
--- Phase 2/functions.h
+++ Phase 2/functions.h
@@ -6,5 +6,18 @@
 
 Program* makeNode(char* id, ProgBlock* progBlock);
 ProgBlock* addProgBlock(VarPart* varPart, FuncPart* funcPart);
+VarPart *addVarPart(VarPart *varPart, VarDecl *varDecl);
+VarDecl *addVarDecl(char *first_id, char *last_id, IdStruct *id);
+IdStruct *addIdStruct(IdStruct* next, char *id);
+VarParams *addVarParams(char *first_id, char *last_id, IdStruct *id);
+Params *addParams(char *first_id, char *last_id, IdStruct *id);
+FuncParams *addFuncParams_Params(Params *params);
+FuncParams *addFuncParams_VarParams(VarParams *varParams);
+FuncDecl *addFuncDecl(char *first_id, char *last_id, FuncParams *funcParams);
+FuncDef *addFuncDef(FuncDecl *funcDecl, VarPart *varPart, Statements stat);
+FuncDef2 *addFuncDef2(char *id, VarPart *varPart, Statements stat);
+FuncPart *addFuncPart_FuncDecl(FuncPart *next, FuncDecl *funcDecl);
+FuncPart *addFuncPart_FuncDef(FuncPart *next, FuncDef *funcDef);
+FuncPart *addFuncPart_FuncDef2(FuncPart *next, FuncDef2 *funcDef2);
 
 #endif
