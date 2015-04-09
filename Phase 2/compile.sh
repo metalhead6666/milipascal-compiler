9aa2d81d86d834d9e878b6b01131ba67151df41b
metalhead6666 <noe.godinho.92@gmail.com>
2015-04-09 22:26:19 +0100

    Removed print files.

--- Phase 2/compile.sh
+++ Phase 2/compile.sh
@@ -1,4 +1,4 @@
 lex mpaparser.l 
 yacc -v -d mpaparser.y 
 cc -o mpaparser *.c *.h -ll -ly -g
-zip mpaparser.zip mpaparser.l mpaparser.y structures.h functions.c functions.h print.c print.h clean.c clean.h
+zip mpaparser.zip mpaparser.l mpaparser.y structures.h functions.c functions.h print.c print.h
