b219d473b2853a0e5195be55d50040a23bde2bf6
metalhead6666 <noe.godinho.92@gmail.com>
2015-04-30 13:02:22 +0100

    Created error structure and enum

--- Phase 3/semantic.h
+++ Phase 3/semantic.h
@@ -1,4 +1,24 @@
 #ifndef _SEMANTIC_
-#define _SEMANTIC_
+#define _SEMANTIC
+
+typedef enum _ErrorType{
+	CannotWriteValues,
+	FunctionIdentifierExpected,
+	IncompatibleTypeArgument,
+	IncompatibleTypeAssignment,
+	IncompatibleTypeStatement,
+	CannotAppliedType,
+	CannotAppliedType2,
+	VariableIdentifierExpected,
+	WrongNumberArguments
+}ErrorType;
+
+typedef struct _ErrorStruct{
+	int number;
+	char *tok_stat;
+	char *type1;
+	char *type2;
+	ErrorType errorType;
+}ErrorStruct;
 
 #endif
