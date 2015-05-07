1eb96c98f2d639cb264e2c434b1aa554b077af96
metalhead6666 <noe.godinho.92@gmail.com>
2015-05-07 02:28:27 +0100

    Updated

---
+++ Phase 4/Test cases/simple_if_then.pas
@@ -0,0 +1,17 @@
+program example ( input , output );
+
+var x, y, greater: integer ;
+
+
+begin
+read(x) ;
+read(y) ;
+
+if (x<=y) then
+   greater := 33
+else
+   greater := 444;
+
+write (greater)
+
+end.
