e5be3bf8b429cd3c754321a612f8189b388b21b2
Christophe235 <christophe_1011@hotmail.com>
2015-05-25 23:50:44 +0100

    merda que tinha

--- Phase 4/generator.h
+++ Phase 4/generator.h
@@ -6,16 +6,37 @@
 #include <string.h>
 #include "functions.h"
 
+/*Functions*/
+void generateProgram(Program* program);
+void generateFunctions(char* type, char* name, program);
+char *varType(char *type);
+
+
 void generateProgram(Program* program){
-	if(program != NULL){
-		if(strcmp(program->type,"WriteLn")==0){
-			printf("WriteLn\n");
-		}
 
+	generateFunctions("i32", "main", program);
 
+	if(program != NULL){	
 		generateProgram(program->son);
 		generateProgram(program->brother);
 	}
+}
+
+void generateFunctions(char* type, char* name, program){
+	printf("define %s @%s() {\n", type, name);
+}
+
+char *varType(char *type){
+	if(strcmp(type, "integer") == 0)
+		return "i32";
+
+	if(strcmp(type, "boolean") == 0)
+		return "i1";
+
+	if(strcmp(type, "real") == 0)
+		return "double";
+
+	return NULL;
 }
 
 #endif
